d066cf1e73d42c1ed28767ccdb32383262b2a32a
tanveer <mdtanviralamht731@gmail.com>
2022-02-25 04:41:35 +0600

    validation added

--- js/main.js
+++ js/main.js
@@ -5,39 +5,53 @@ const foodDetails = document.getElementById('items-details');
 const searchMeals = ()=>{
     const field = document.getElementById('search-field')
     const fieldValue = field.value;
+    const error  = document.getElementById('error-message');
+    if(field.value ==""){
+        error.innerText = "Please Enter your Food items";
+    }else{
     field.value = "";
+    error.innerText ="";
     const apiUrl = `https://themealdb.com/api/json/v1/1/search.php?s=${fieldValue}`;
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => getMeals(data.meals))
+    }
 }
 const getMeals= meals =>{
     // console.log(meals);
     container.innerHTML = "";
     foodDetails.innerHTML = "";
-    meals.forEach(meal => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        const des = meal.strInstructions;
-        const desConvert= des.slice(0, 100);
-        div.innerHTML = `
-        <div class="card shadow border">
-            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-            <div class="card-body">
-                    <h5 class="card-title">${meal.strMeal}</h5>
-                    <p class="card-text">${desConvert}</p>
-                    <div class= "row  row-cols-md-2 g-2">
-                    <a href="${meal.strSource}" target ="_blank">
-                        <button type="button" class="btn btn-outline-primary">Order Now</button>
-                    </a>
-                    <button onclick="details(${meal.idMeal})" type="button" class="btn btn-outline-primary">See Details</button>
-                    </div>
-   
+    const empty = document.getElementById('empty');
+    console.log(empty);
+    console.log(meals);
+
+    // if(meals.length == null){
+    //     empty.innerText = "Your Food Is not Available!";
+    // }
+        meals.forEach(meal => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            const des = meal.strInstructions;
+            const desConvert= des.slice(0, 100);
+            div.innerHTML = `
+            <div class="card shadow border">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                        <p class="card-text">${desConvert}</p>
+                        <div class= "row  row-cols-md-2 g-2">
+                        <a href="${meal.strSource}" target ="_blank">
+                            <button type="button" class="btn btn-outline-primary">Order Now</button>
+                        </a>
+                        <button onclick="details(${meal.idMeal})" type="button" class="btn btn-outline-primary">See Details</button>
+                        </div>
+    
+                </div>
             </div>
-        </div>
-        `;
-        container.appendChild(div);
-    });
+            `;
+            container.appendChild(div);
+        });
+    
 }
 // get food details function 
 const details=(meals)=>{
